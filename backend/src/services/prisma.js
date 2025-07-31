575fcbcfb18fdade1114d31b77a9d9946373b9b2
NASVIPS <vips.nas.management@gmail.com>
2025-07-31 15:23:53 +0530

    reverting prisma.js

--- backend/src/services/prisma.js
+++ backend/src/services/prisma.js
@@ -1,14 +1,5 @@
 // rfid-attendance-system/apps/backend/src/services/prisma.js
 import { PrismaClient } from '@prisma/client';
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma || prismaClientSingleton();
+const prisma = new PrismaClient();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
