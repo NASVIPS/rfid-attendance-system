f5c588b8587a8cec4bf3b4c1eea5a8a43364d026
NASVIPS <vips.nas.management@gmail.com>
2025-07-31 15:06:53 +0530

    making it serverless

--- backend/src/services/prisma.js
+++ backend/src/services/prisma.js
@@ -1,5 +1,14 @@
 // rfid-attendance-system/apps/backend/src/services/prisma.js
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
